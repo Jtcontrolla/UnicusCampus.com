d9d75b7dba2150621e68f7e7215aac2345145698
Usiaphe <usiapheblessing116@gmail.com>
2025-03-07 16:19:26 +0100

    work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { react } from 'react'
-import './App.css'
+
 
 function App() {
   return (
     <>
-      <div>
-        Lets Start Work, Everybody clone or pull and start working on your sections
-      </div>
+    
     </>
   )
 }
---
+++ src/Components/Programs/Programs.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Programs.css'
+
+const Progarms = () => {
+  return (
+    <div class="container">
+    <h1>Contrary To Popular Belief</h1>
+    
+    <div class="logo-section">
+        <div class="logo">YOUR<span>COMPANY</span><small>Your Slogan Here</small></div>
+        <div class="logo">YOUR<span>COMPANY</span><small>Your Slogan Here</small></div>
+        <div class="logo">YOUR<span>COMPANY</span><small>Your Slogan Here</small></div>
+        <div class="logo">YOUR<span>COMPANY</span><small>Your Slogan Here</small></div>
+    </div>
+
+    <h2>Variations Passag</h2>
+
+    <div class="content">
+        <div class="text">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book It has.</p>
+            <button>Simply Random!</button>
+        </div>
+        <div class="image-placeholder"></div>
+    </div>
+</div>
+  )
+}
+
+export default Progarms
