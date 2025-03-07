aeb50745f251ab31c31a0145863cac63bcbbf72c
Johnsonlight002 <li7ade002@gmail.com>
2025-03-07 16:00:03 +0000

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './hero.css'
+const Hero = () => {
+  return (
+    <div className='bodys'>
+      <div>
+        <h1>Contrary To The</h1>
+        <h1>Popular Belief, At</h1>
+      </div>
+      <div>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply.</p>
+      </div>
+      <div className='button'>
+        <p className='non'>Non Charact</p>
+        <p className='lorem'>Lorem Ipsum</p>
+      </div>
+      <div>
+        <p>There are many variad</p>
+        <p>tions</p>
+      </div>
+      <div className='cir'>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+        <p className='circle'></p>
+       
+      </div>
+    </div>
+  )
+}
+
+export default Hero
