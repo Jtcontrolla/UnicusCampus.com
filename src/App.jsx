9890f9264162ae1af4bcc7197713cc78b7626430
kparobookus <kparobookus@gmail.com>
2025-03-10 03:12:30 -1100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { React } from 'react'
 import Header from './components/Header.jsx'
+import Testimonial from './components/testimonial/Testimonial.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Testimonial />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Testimonial from './components/testimonial/Testimonial.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <>
-    <Testimonial></Testimonial>
-    </>
-    
-    
   </StrictMode>,
 )
