b306c9699e057179a44d0658ff25fc5ebfcccb47
Diitech <joyoke06@gmail.com>
2025-03-10 14:38:30 +0000

    Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,46 @@
 import React from "react";
 import "./footer.css";
-import  Unicus_icons  from '../assets/unicus_icon.png'
+import Unicus_icon  from "./../../assets/Unicus_icon.png";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 
-
 const Footer = () => {
   return (
     <section className="footer-section">
       <div className="footer-container">
-    
         <div className="footer-content">
-          
           <div className="footer-left">
-            <div className='footer-leftleft'>
-          <img src={Unicus_icons} alt="Unicus" className="footer-logo" />
+            <div className="footer-leftleft">
+              <img src={Unicus_icon} alt="Unicus" className="footer-logo" />
 
-            <h2 className="company-name">UNICUS Live</h2>
+              <h2 className="company-name">UNICUS Live</h2>
             </div>
             <p className="company-tagline">Learn</p>
-         
+
             <div className="footer-social">
-              <a href="#"><FaFacebook /><i className="fab fa-facebook-f"></i></a>
-              <a href="#"><FaTwitter /><i className="fab fa-twitter"></i></a>
-              <a href="#"><FaInstagram /><i className="fab fa-instagram"></i></a>
-              <a href="#"><FaLinkedin /><i className="fab fa-linkedin-in"></i></a>
-              <a href="#"><FaYoutube /><i className="fab fa-youtube"></i></a>
+              <a href="#">
+                <FaFacebook />
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <FaTwitter />
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <FaInstagram />
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <FaLinkedin />
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+              <a href="#">
+                <FaYoutube />
+                <i className="fab fa-youtube"></i>
+              </a>
             </div>
           </div>
 
@@ -36,8 +48,9 @@ const Footer = () => {
             <div className="footer-column">
               <h3>Classes</h3>
               <ul>
-                <li>Web Design <br>
-                </br>and Programming</li>
+                <li>
+                  Web Design <br></br>and Programming
+                </li>
                 <li>Cybersecurity</li>
                 <li>Product Deign</li>
                 <li>Data Analysis</li>
@@ -55,13 +68,16 @@ const Footer = () => {
             </div>
           </div>
 
-       
           <div className="footer-right">
             <h3>Time</h3>
             <p>8am Prompt</p>
             <p>All Classes are Active NOW!!</p>
-            <p><i className="fas fa-phone-alt"></i> +234 706 611 2211</p>
-            <p><i className="fas fa-envelope"></i> support@unicus.live</p>
+            <p>
+              <i className="fas fa-phone-alt"></i> +234 706 611 2211
+            </p>
+            <p>
+              <i className="fas fa-envelope"></i> support@unicus.live
+            </p>
           </div>
         </div>
 
