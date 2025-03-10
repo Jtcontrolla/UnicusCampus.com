47bbb7b4f1c651c9ef0a5a62764101edcf467d00
Johnsonlight002 <li7ade002@gmail.com>
2025-03-10 14:27:12 +0000

    second name

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { react } from 'react'
+import Hero from './components/Hero.jsx'
 function App() {
   return (
     <>
       <div>
-
+      <Hero></Hero>
       </div>
     </>
   )
 }
-
 export default App
